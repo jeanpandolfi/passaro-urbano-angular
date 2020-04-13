510de704c942214b1d225cbb51d0ef7c17896cc8
jeanpandolfi <jean-pandolfi@hotmail.com>
2020-04-13 19:02:40 -0300

    add componente oferta e definindo os roteamento entre componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component'
 import { RestauranteComponent } from './restaurante/restaurante.component'
 import { DiversaoComponent } from './diversao/diversao.component'
+import { OfertaComponent } from './oferta/oferta.component'
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch:'prefix'},
   {path: 'home', component: HomeComponent},
   {path: 'restaurantes', component: RestauranteComponent},
   {path: 'diversao', component: DiversaoComponent},
+  {path: 'ofertas', component: HomeComponent},
+  {path: 'ofertas/:id', component: OfertaComponent},
 ];
 
 @NgModule({
